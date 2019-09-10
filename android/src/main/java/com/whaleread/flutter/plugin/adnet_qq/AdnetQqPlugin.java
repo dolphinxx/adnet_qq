5466252c5f5fe7159203f19c1d3cc14dc4d22762
dolphin <adaikiss@163.com>
2019-09-10 15:27:02 +0800

    missing break in case statement

--- android/src/main/java/com/whaleread/flutter/plugin/adnet_qq/AdnetQqPlugin.java
+++ android/src/main/java/com/whaleread/flutter/plugin/adnet_qq/AdnetQqPlugin.java
@@ -88,6 +88,7 @@ public class AdnetQqPlugin implements MethodCallHandler {
           splashAd.close();
           splashAd = null;
         }
+        break;
       }
       default:
         result.notImplemented();
