ae4ca9f22987f83d95b49d643baf4b8b20e342e4
adaikiss@163.com <81656583>
2020-12-15 16:02:51 +0800

    plugin need to be initialized only once.

--- lib/src/adnet_qq_plugin.dart
+++ lib/src/adnet_qq_plugin.dart
@@ -10,6 +10,10 @@ class AdnetQqPlugin {
   static const MethodChannel channel =
       const MethodChannel(PLUGIN_ID);
 
+  static bool _initialized = false;
+
+  static bool get initialized => _initialized;
+
   /// [appId] appId from `adnet.qq.com`
   ///
   /// [requestReadPhoneState] whether request READ_PHONE_STATE (android only), 0: no, 1: yes, 2: force(exit app when user reject this permission), default 0
@@ -34,7 +38,14 @@ class AdnetQqPlugin {
   /// - 999	其他
   ///
   static Future<bool> config({@required String appId, int requestReadPhoneState: 0, int requestAccessFineLocation: 0, int adChannel}) async {
-    return await channel.invokeMethod('config', {'appId': appId, 'requestReadPhoneState': requestReadPhoneState, 'requestAccessFineLocation': requestAccessFineLocation, 'adChannel': adChannel});
+    if(_initialized) {
+      return true;
+    }
+    bool result = await channel.invokeMethod('config', {'appId': appId, 'requestReadPhoneState': requestReadPhoneState, 'requestAccessFineLocation': requestAccessFineLocation, 'adChannel': adChannel});
+    if(result == true) {
+      _initialized = true;
+    }
+    return result;
   }
 
   /// create a UnifiedInterstitialAd, and communicate with it through channel Id '$PLUGIN_ID/unified_interstitial_$posId'
